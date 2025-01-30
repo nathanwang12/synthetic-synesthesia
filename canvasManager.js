e1f3e607eec08e08fd068a9246ec4b7d2274834a
Nathan Wang <nathanwang@Nathans-MacBook-Pro-2.local>
2025-01-30 14:14:16 -0600

    no longer factoring in opacity

--- canvasManager.js
+++ canvasManager.js
@@ -1,66 +1,84 @@
 class CanvasManager {
-  constructor(canvasElement, colorPalette, brushSizeSlider, brushOpacitySlider, resetButton) {
-    this.canvas = canvasElement;
-    this.ctx = this.canvas.getContext('2d');
-    this.colorPalette = colorPalette;
-    this.brushSizeSlider = brushSizeSlider;
-    this.brushOpacitySlider = brushOpacitySlider;
-    this.resetButton = resetButton;
+  constructor(canvasElement, colorPalette, brushSizeSlider, clearButton) {
+      this.canvas = canvasElement;
+      this.canvas.width = this.canvas.offsetWidth;
+      this.canvas.height = this.canvas.offsetHeight;
+      this.ctx = this.canvas.getContext('2d');
+      this.ctx.lineJoin = 'round';
+      this.ctx.lineCap = 'round';
+      this.colorPalette = colorPalette;
+      this.slider = {
+          size: brushSizeSlider
+      }
+      this.clearButton = clearButton;
+      this.painting = false;
+      this.brush = {
+          color: '#FFFFFF',
+          size: parseInt(this.slider.size.value)
+      }
 
-    this.painting = false;
-
-    this.brushColor = '#FFFFFF'
-    this.brushSize = parseInt(this.brushSizeSlider.value);
-    this.brushOpacity = parseInt(this.brushOpacitySlider);
-    this.setupEvents();
+      this.setupEvents();
   }
 
   setupEvents() {
-    this.canvas.addEventListener('mousedown', () => {
-      this.painting = true;
-    });
-
-    this.canvas.addEventListener('mouseup', () => {
-      this.painting = false;
-      this.ctx.beginPath();
-    });
-
-    this.canvas.addEventListener('mousemove', (event) => {
-      if (this.painting) {
-        this.draw(event.offsetX, event.offsetY);
-      }
-    });
-
-    this.colorPalette.querySelectorAll('.color-box').forEach((box) => {
-      box.addEventListener('click', (event) => {
-        this.brushColor = event.target.dataset.color;
-        this.updateCursor();
+      this.canvas.addEventListener("mousedown", (e) => {
+          this.startPainting(e);
       });
-    });
 
-    this.brushSizeSlider.addEventListener('input', (event) => {
-      this.brushSize = parseInt(event.target.value);
-    });
+      this.canvas.addEventListener("mousemove", (e) => {
+          if (!this.painting) return;
+          this.paint(e);
+      });
 
-    this.brushOpacitySlider.addEventListener('input', (event) => {
-      this.brushOpacity = parseInt(event.target.value);
-    })
+      this.canvas.addEventListener("mouseup", () => {
+          this.endPainting();
+      });
 
-    this.resetButton.addEventListener('click', () => this.clearCanvas());
+      this.canvas.addEventListener("mouseout", () => {
+          this.endPainting();
+      });
+
+      this.colorPalette.querySelectorAll('.color-box').forEach((box) => {
+          box.addEventListener('click', (event) => {
+              this.brush.color = event.target.dataset.color;
+              // Update selected color visual feedback
+              this.colorPalette.querySelectorAll('.color-box').forEach(b => {
+                  b.classList.remove('selected');
+              });
+              event.target.classList.add('selected');
+          });
+      });
+
+      this.slider.size.addEventListener("input", (e) => {
+          this.brush.size = parseInt(e.target.value);
+      });
+
+      this.clearButton.addEventListener("click", () => {
+          this.clearCanvas();
+      });
   }
 
-  draw(x, y) {
-    this.ctx.lineWidth = this.brushSize;
-    this.ctx.lineCap = 'round';
-    this.ctx.strokeStyle = this.brushColor || '#FFFFFF'; // Default to black if no color selected
+  startPainting(e) {
+      this.painting = true;
+      this.ctx.beginPath();
+      this.ctx.moveTo(e.offsetX, e.offsetY);
+      this.ctx.strokeStyle = this.brush.color;
+      this.ctx.lineWidth = this.brush.size;
+  }
 
-    this.ctx.lineTo(x, y);
-    this.ctx.stroke();
-    this.ctx.beginPath();
-    this.ctx.moveTo(x, y);
+  paint(e) {
+      this.ctx.lineTo(e.offsetX, e.offsetY);
+      this.ctx.stroke();
+      this.ctx.beginPath();
+      this.ctx.moveTo(e.offsetX, e.offsetY);
+  }
+
+  endPainting() {
+      this.painting = false;
+      this.ctx.beginPath();
   }
 
   clearCanvas() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
